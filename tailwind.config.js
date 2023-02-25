153e13790f769b963075ede0e2b7fd638b990c44
Aman <amanpandey1910@gmail.com>
2023-02-25 19:22:28 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      backgroundImage: {
+        'landing_background': "url('TuFodega/elements/background.png')"
+      }
+    },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+
+  },
+  plugins: [],
+}
